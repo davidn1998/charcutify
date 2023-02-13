0521fc18d1323663c215b806e69c752553ec7945
David Nwachukwu <davidnw1998@gmail.com>
2023-02-13 07:54:59 +0000

    add touch gesture to carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,6 +4,7 @@ import {
   CarouselItem5,
 } from "@/components/CarouselItems";
 import { ICarouselItem } from "@/types";
+import { useDrag } from "@use-gesture/react";
 import { useState } from "react";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
@@ -21,7 +22,23 @@ export const Carousel = ({
   const [offset, setOffset] = useState(offsetInit);
   const [disabled, setDisabled] = useState(false);
 
-  const handleNext = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const bind = useDrag(
+    ({ active, direction: [xDir], cancel }) => {
+      if (active && xDir > 0) {
+        handleNext();
+        cancel();
+      } else if (active && xDir < 0) {
+        handlePrev();
+        cancel();
+      }
+    },
+    {
+      threshold: 100,
+      enabled: !disabled,
+    }
+  );
+
+  const handleNext = () => {
     setDisabled(true);
     let newoffset = 0;
     if (offset === carouselItems.length - 1) {
@@ -35,7 +52,7 @@ export const Carousel = ({
     }, 700);
   };
 
-  const handlePrev = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handlePrev = () => {
     setDisabled(true);
     let newoffset = 0;
     if (offset === 0) {
@@ -52,7 +69,7 @@ export const Carousel = ({
   const getPos = (currPos: number) => {
     let newPos = 0;
 
-    if (currPos + offset >= carouselItems.length && carouselItems.length > 1) {
+    if (currPos + offset >= carouselItems.length) {
       newPos = currPos + offset - carouselItems.length;
     } else {
       newPos = currPos + offset;
@@ -64,13 +81,16 @@ export const Carousel = ({
   return (
     <div className="flex h-[700px] items-center justify-center">
       <button
-        onClick={(e) => handlePrev(e)}
-        disabled={disabled || carouselItems.length === 1}
+        onClick={() => handlePrev()}
+        disabled={disabled}
         className="bg-neutral-600 p-3 text-3xl transition-all duration-500 hover:bg-white hover:text-neutral-900"
       >
         <MdKeyboardArrowLeft />
       </button>
-      <div className="relative mx-auto flex h-[400px] w-[75vw] justify-center md:h-[600px]">
+      <div
+        {...bind()}
+        className="relative mx-auto flex h-[400px] w-[75vw] touch-pan-y justify-center md:h-[600px]"
+      >
         {carouselItems.map((item, i) => {
           let carousel = <></>;
           switch (displayNumber) {
@@ -118,8 +138,8 @@ export const Carousel = ({
         })}
       </div>
       <button
-        onClick={(e) => handleNext(e)}
-        disabled={disabled || carouselItems.length === 1}
+        onClick={() => handleNext()}
+        disabled={disabled}
         className="bg-neutral-600 p-3 text-3xl transition-all duration-500 hover:bg-white hover:text-neutral-900"
       >
         <MdKeyboardArrowRight />
